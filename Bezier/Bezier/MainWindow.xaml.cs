b2e566a3663d5b0ee9beb62d6104276e28759607
Jakub Modzelewski <49248797+Modzel200@users.noreply.github.com>
2024-11-13 21:14:16 +0100

    projekty

---
+++ Bezier/Bezier/MainWindow.xaml.cs
@@ -0,0 +1,260 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bezier;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    
+    private int degree = 3; 
+    private List<Point> controlPoints;
+    private Point selectedPoint;
+    private int selectedPointIndex;
+    private bool isDragging = false;
+    
+    public MainWindow()
+    {
+        InitializeComponent();
+        controlPoints = new List<Point>();
+        degreeTextBox.Text = degree.ToString();
+        pointListBox.ItemsSource = controlPoints;
+        DataContext = this;
+        DrawPoints();
+        Closed += Window_Closed;
+    }
+
+     private void Canvas_MouseMove(object sender, MouseEventArgs e)
+    {
+        if (isDragging && e.LeftButton == MouseButtonState.Pressed)
+        {
+            int indexOfPoint = controlPoints.IndexOf(selectedPoint);
+            if (indexOfPoint == -1) return;
+
+            Point mousePosition = e.GetPosition(canvas);
+            selectedPoint.X = mousePosition.X;
+            selectedPoint.Y = mousePosition.Y;
+            controlPoints[indexOfPoint] = selectedPoint;
+            RefreshCanvas();
+        }
+    }
+
+    private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
+    {
+        isDragging = false;
+    }
+
+    private void SetPointClick(object sender, MouseButtonEventArgs e)
+    {
+        Point mousePosition = e.GetPosition(canvas);
+        
+        bool existsPoint = controlPoints.Any(p => CalculateDistance(p, mousePosition) < 10);
+
+        if (int.TryParse(degreeTextBox.Text, out int newDegree))
+        {
+            degree = newDegree;
+        }
+
+        if (!existsPoint)
+        {
+            if (degree >= controlPoints.Count)
+            {
+                controlPoints.Add(new Point(mousePosition.X, mousePosition.Y));
+            }
+            else
+            {
+                MessageBox.Show($"Za dużo punktów, Stopień równy {degree} pozwala na dodanie {degree + 1} punktów");
+            }
+        }
+        else
+        {
+            isDragging = true;
+            selectedPoint = controlPoints.First(p => CalculateDistance(p, mousePosition) < 10);
+        }
+
+        RefreshCanvas();
+    }
+
+    private void pointListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (pointListBox.SelectedItem != null)
+        {
+            Point selectedPoint = (Point)pointListBox.SelectedItem;
+            XTextBox.Text = selectedPoint.X.ToString("F2");
+            YTextBox.Text = selectedPoint.Y.ToString("F2");
+            selectedPointIndex = controlPoints.IndexOf(selectedPoint);
+        }
+    }
+
+    public void DrawPoints()
+    {
+        foreach (var p in controlPoints)
+        {
+            Ellipse ellipse = new Ellipse
+            {
+                Fill = Brushes.DarkRed,  
+                Width = 10,  
+                Height = 10,
+            };
+            Canvas.SetLeft(ellipse, p.X - ellipse.Width / 2);
+            Canvas.SetTop(ellipse, p.Y - ellipse.Height / 2);
+            canvas.Children.Add(ellipse);
+        }
+        pointListBox.Items.Refresh();
+    }
+
+    private void DrawBezierCurve()
+    {
+        canvas.Children.Clear();
+        Path bezierPath = new Path
+        {
+            Stroke = Brushes.DarkBlue,  
+            StrokeThickness = 4  
+        };
+
+        PathGeometry pathGeometry = new PathGeometry();
+        PathFigure pathFigure = new PathFigure
+        {
+            StartPoint = controlPoints.FirstOrDefault()  
+        };
+
+        PolyBezierSegment polyBezierSegment = new PolyBezierSegment
+        {
+            Points = new PointCollection(CalculateBezierPoints())
+        };
+
+        pathFigure.Segments.Add(polyBezierSegment);
+        pathGeometry.Figures.Add(pathFigure);
+        bezierPath.Data = pathGeometry;
+
+        canvas.Children.Add(bezierPath);
+    }
+
+    private PointCollection CalculateBezierPoints()
+    {
+        PointCollection bezierPoints = new PointCollection();
+
+        for (double i = 0; i <= 1; i += 0.01)  
+        {
+            Point bezierPoint = CalculateBezierPoint(i);
+            bezierPoints.Add(bezierPoint);
+        }
+        
+        for (int i = 0; i < bezierPoints.Count - 1; i++)
+        {
+            Line line = new Line
+            {
+                X1 = bezierPoints[i].X,
+                Y1 = bezierPoints[i].Y,
+                X2 = bezierPoints[i + 1].X,
+                Y2 = bezierPoints[i + 1].Y,
+                Stroke = Brushes.Gray,
+                StrokeThickness = 2  
+            };
+
+            canvas.Children.Add(line);
+        }
+
+        return bezierPoints;
+    }
+
+    private Point CalculateBezierPoint(double t)
+    {
+        int n = controlPoints.Count - 1;
+        double x = 0;
+        double y = 0;
+
+        for (int i = 0; i <= n; i++)
+        {
+            double berstein = Bernstein(n, i, t);
+            x += berstein * controlPoints[i].X;
+            y += berstein * controlPoints[i].Y;
+        }
+
+        return new Point(x, y);
+    }
+
+    private double Bernstein(int n, int i, double t)
+    {
+        return Newton(n, i) * Math.Pow(1.0 - t, n - i) * Math.Pow(t, i);
+    }
+
+    private double CalculateDistance(Point point1, Point point2)
+    {
+        double deltaX = point2.X - point1.X;
+        double deltaY = point2.Y - point1.Y;
+        return Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+    }
+
+    private void ClearCanvas(object sender, RoutedEventArgs e)
+    {
+        canvas.Children.Clear();
+        controlPoints.Clear();
+    }
+
+    private void AddPoint_Click(object sender, RoutedEventArgs e)
+    {
+        if (degree >= controlPoints.Count && double.TryParse(XTextBox.Text, out double X) && double.TryParse(YTextBox.Text, out double Y))
+        {
+            controlPoints.Add(new Point(X, Y));
+        }
+        else
+        {
+            MessageBox.Show($"Za dużo punktów, Stopień równy {degree} pozwala na dodanie {degree + 1} punktów");
+        }
+        RefreshCanvas();
+    }
+
+    private void EditPoint_Click(object sender, RoutedEventArgs e)
+    {
+        if (pointListBox.SelectedItem != null)
+        {
+            Point selectedPoint = (Point)pointListBox.SelectedItem;
+            selectedPoint.X = double.Parse(XTextBox.Text);
+            selectedPoint.Y = double.Parse(YTextBox.Text);
+            controlPoints[selectedPointIndex] = selectedPoint;
+
+            RefreshCanvas();
+        }
+        else
+        {
+            MessageBox.Show("Wybierz punkt do edycji");
+        }
+    }
+
+    private void RefreshCanvas()
+    {
+        pointListBox.Items.Refresh();
+        canvas.Children.Clear();
+        DrawBezierCurve();
+        DrawPoints();
+    }
+
+    private void Window_Closed(object sender, EventArgs e)
+    {
+        GC.SuppressFinalize(this);
+        MainWindow mainWindow = new MainWindow();
+        mainWindow.Show();
+        this.Close();
+    }
+
+    private int Newton(int n, int i)
+    {
+        int result = 1;
+        for (int j = 1; j <= i; j++)
+        {
+            result = result * (n - j + 1) / j;
+        }
+        return result;
+    }
+}
---
+++ Transformacja2D/Transformacja2D/MainWindow.xaml.cs
@@ -0,0 +1,477 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+using Microsoft.Win32;
+
+namespace Transformacja2D;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private bool isDrawing = false;
+    private bool isDragging = false;
+    private Point startPoint;
+    private Polygon currentPolygon;
+    private List<Polygon> shapes = new List<Polygon>();
+    private List<Point> points = new List<Point>();
+    Point centerOfPolygon;
+    public MainWindow()
+    {
+        InitializeComponent();
+        Load("F:\\Github\\Grafika-master\\Data\\DATA.xml");
+
+        Closed += Window_Closed;
+
+        //List////////////////////////////////////////////////////////////////////////////////////////////////////
+        pointListBox.ItemsSource = points;
+        DataContext = this;
+        this.WindowState = WindowState.Maximized;
+    }
+
+
+    private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            canvas.Focus();
+            startPoint = e.GetPosition(canvas);
+            currentPolygon = GetShapeUnderMouse();
+
+            if (currentPolygon != null)
+            {
+                isDragging = true;
+                isDrawing = false;
+            }
+            else
+            {
+                isDrawing = true;
+                isDragging = false;
+            }
+
+        }
+
+        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (isDrawing)
+            {
+                Point mousePoint = e.GetPosition(canvas);
+
+                if (e.LeftButton == MouseButtonState.Pressed)
+                {
+                    points.Add(mousePoint);
+                    DrawPoints();
+                    pointListBox.Items.Refresh();
+                }
+
+            }
+            if (e.RightButton == MouseButtonState.Pressed)
+            {
+                Draw();
+                ClearPoints();
+            }
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging & currentPolygon != null)
+            {
+                Point currentPoint = Mouse.GetPosition(this);
+                double deltaX = currentPoint.X - startPoint.X;
+                double deltaY = currentPoint.Y - startPoint.Y;
+
+                if (Keyboard.IsKeyDown(Key.LeftCtrl))
+                {
+                    centerOfPolygon = new Point((canvas.ActualWidth / 2), canvas.ActualHeight / 2);
+                    double alfa = Math.Atan2(deltaY, deltaX);
+                    Rotation(alfa, centerOfPolygon);
+                }
+                else if (Keyboard.IsKeyDown(Key.LeftAlt))
+                {
+                    double scale = 1 + (deltaY / 100.0); // Dostosuj wartość skali do potrzeb
+                    Scale(scale, currentPoint);
+                }
+                else
+                {
+                    var shape = currentPolygon;
+
+                    for (int i = 0; i < shape.Points.Count; i++)
+                    {
+                        shape.Points[i] = new Point(shape.Points[i].X + deltaX, shape.Points[i].Y + deltaY);
+                    }
+                }
+                startPoint = currentPoint;
+            }
+        }
+
+        private void Canvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            isDrawing = false;
+            isDragging = false;
+            //currentPolygon = GetShapeUnderMouse(startPoint);
+        }
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void AddPoint()
+        {
+            try
+            {
+                double x = Convert.ToDouble(XTextBox.Text);
+                double y = Convert.ToDouble(YTextBox.Text);
+                points.Add(new Point(x, y));
+                pointListBox.Items.Refresh();
+                DrawPoints();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Nieprawidłowe wartości wprowadzone do dodania punktu");
+            }
+        }
+
+        private void Draw()
+        {
+            if(points.Count < 3)
+            {
+                MessageBox.Show("Za mało punktów do utworzenia kształtu. Dodaj conajmniej 3");
+                return;
+            }
+            Polygon polygon = new Polygon
+            {
+                Points = new PointCollection(points),
+                Stroke = Brushes.Black,
+                StrokeThickness = 3,
+                Fill = Brushes.DarkCyan,
+            };
+            shapes.Add(polygon);
+            canvas.Children.Add(polygon);
+            points.Clear();
+            pointListBox.Items.Refresh();
+        }
+
+        private void Transform(double h, double v)
+        {
+            try
+            {
+                var shape = currentPolygon as Polygon;
+                List<Point> rotatedPoints = new List<Point>();
+                foreach (var point in shape.Points)
+                {
+
+                    double x = point.X + h;
+                    double y = point.Y + v;
+
+                    rotatedPoints.Add(new Point(x, y));
+
+                }
+                shape.Points = new PointCollection(rotatedPoints);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Nieprawidłowe wartości wprowadzone do przesunięcia");
+            }
+        }
+
+        private void Rotation(double alfa, Point givenPoint)
+        {
+            try
+            {
+                var shape = currentPolygon;
+                if(shape == null)
+                {
+                    MessageBox.Show("Nie wybrano kształtu");
+                    return;
+                }
+                List<Point> rotatedPoints = new List<Point>();
+                var rad = (alfa * (Math.PI)) / 180;
+                foreach (var point in shape.Points)
+                {
+                    double translatedX = point.X - givenPoint.X;
+                    double translatedY = point.Y - givenPoint.Y;
+
+                    double x = givenPoint.X + translatedX * Math.Cos(rad) - translatedY * Math.Sin(rad);
+                    double y = givenPoint.Y + translatedX * Math.Sin(rad) + translatedY * Math.Cos(rad);
+
+                    rotatedPoints.Add(new Point(x, y));
+
+                }
+                shape.Points = new PointCollection(rotatedPoints);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Nieprawidłowe wartości wprowadzone do obrócenia");
+            }
+        }
+
+        private void Scale(double k, Point givenPoint)
+        {
+            try
+            {
+                var shape = currentPolygon as Polygon;
+                if (shape == null)
+                {
+                    MessageBox.Show("Nie wybrano kształtu");
+                    return;
+                }
+                List<Point> rotatedPoints = new List<Point>();
+                foreach (var point in shape.Points)
+                {
+                    double x = point.X * k + (1 - k) * givenPoint.X;
+                    double y = point.Y * k + (1 - k) * givenPoint.Y;
+
+                    rotatedPoints.Add(new Point(x, y));
+
+                }
+                shape.Points = new PointCollection(rotatedPoints);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Nieprawidłowe wartości wprowadzone do skalowania");
+            }
+        }
+
+        private void AddPointButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddPoint();
+        }
+
+        private void AddPolygonButton_Click(object sender, RoutedEventArgs e)
+        {
+            Draw();
+            ClearPoints();
+        }
+
+
+        private void TranslationButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(translationXTextBox.Text, out double h) & double.TryParse(translationYTextBox.Text, out double v))
+            {
+                Transform(h, v);
+            }
+        }
+
+        private void RotationButton_Click(object sender, RoutedEventArgs e)
+        {
+            centerOfPolygon = new Point((canvas.ActualWidth / 2), canvas.ActualHeight / 2);
+            if (double.TryParse(translationXTextBox.Text, out double x) & double.TryParse(translationYTextBox.Text, out double y) & double.TryParse(rotationAngleTextBox.Text, out double alfa))
+            {
+                Point givenPoint = new Point(x, y);
+                Rotation(alfa, givenPoint);
+
+            }
+            else if (alfa != 0)
+            {
+                Rotation(alfa, centerOfPolygon);
+            }
+            else
+            {
+                MessageBox.Show("Nieprawidłowe wartości wprowadzone do obrócenia");
+            }
+        }
+
+        private void ScalingButton_Click(object sender, RoutedEventArgs e)
+        {
+            centerOfPolygon = new Point((canvas.ActualWidth / 2), canvas.ActualHeight / 2);
+            if (double.TryParse(translationXTextBox.Text, out double x) & double.TryParse(translationYTextBox.Text, out double y) & double.TryParse(scalingFactorTextBox.Text, out double k))
+            {
+                Point givenPoint = new Point(x, y);
+                Scale(k, givenPoint);
+            }
+            else if (k != 0)
+            {
+                Scale(k, centerOfPolygon);
+            }
+            else
+            {
+                MessageBox.Show("Nieprawidłowe wartości wprowadzone do skalowania");
+            }
+        }
+
+        /////Zapis i odczyt z pliku//////////////////////////////////////////////////////////////////////////////////////////////////
+        private void Save(string fileName)
+        {
+            List<PolygonData> shapeDataList = new List<PolygonData>();
+
+            foreach (var shape in shapes)
+            {
+                var pointsList = new List<Point>();
+                foreach (var point in shape.Points)
+                {
+                    pointsList.Add(point);
+                }
+
+                shapeDataList.Add(new PolygonData
+                {
+                    points = pointsList,
+                    FillColor = shape.Fill.ToString(),
+                });
+
+            }
+
+            PolygonList polygonList = new PolygonList { Shapes = shapeDataList };
+
+            using (StreamWriter sw = new StreamWriter(fileName))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(PolygonList));
+                serializer.Serialize(sw, polygonList);
+            }
+        }
+
+        private void Load(string fileName)
+        {
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(PolygonList));
+                PolygonList polygonList = (PolygonList)serializer.Deserialize(sr);
+
+                foreach (var shapeData in polygonList.Shapes)
+                {
+                    PolygonData polygonData = (PolygonData)shapeData;
+                    var polygon = new Polygon
+                    {
+                        Points = new PointCollection(polygonData.points),
+                        Fill = (SolidColorBrush)(new BrushConverter().ConvertFrom(polygonData.FillColor)),
+                        Stroke = Brushes.Black,
+                        StrokeThickness = 3,
+                    };
+                    canvas.Children.Add(polygon);
+                    shapes.Add(polygon);
+                }
+            }
+        }
+
+        private void SaveToFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "XML Files|*.xml";
+            if (dialog.ShowDialog() == true)
+            {
+                Save(dialog.FileName);
+            }
+        }
+
+        private void LoadFromFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "XML Files|*.xml";
+            if (dialog.ShowDialog() == true)
+            {
+                Load(dialog.FileName);
+            }
+        }
+        /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        ////Wyszukiwanie istniejących//////////////////////////////////////////////////////////////////////////////////
+        private Polygon GetShapeUnderMouse()
+        {
+            foreach (var shape in canvas.Children)
+            {
+                if (shape is Polygon polygon)
+                {
+                    if (IsPointInsidePolygon(startPoint, polygon.Points))
+                    {
+                        return polygon;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private bool IsPointInsidePolygon(Point point, PointCollection polygonPoints)
+        {
+            int count = polygonPoints.Count;
+            bool inside = false;
+
+            for (int i = 0, j = count - 1; i < count; j = i++)
+            {
+                if (((polygonPoints[i].Y > point.Y) != (polygonPoints[j].Y > point.Y)) &&
+                    (point.X < (polygonPoints[j].X - polygonPoints[i].X) * (point.Y - polygonPoints[i].Y) / (polygonPoints[j].Y - polygonPoints[i].Y) + polygonPoints[i].X))
+                {
+                    inside = !inside;
+                }
+            }
+
+            return inside;
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            canvas.Children.Clear();
+            XTextBox.Text = "";
+            YTextBox.Text = "";
+            translationXTextBox.Text = "";
+            translationYTextBox.Text = "";
+            rotationAngleTextBox.Text = "";
+            scalingFactorTextBox.Text = "";
+            shapes.Clear();
+        }
+
+        private void ClearPointButton_Click(object sender, RoutedEventArgs e)
+        {
+            points.Clear();
+            pointListBox.Items.Refresh();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            GC.SuppressFinalize(this);
+            Save("E:\\Projekty\\Grafika\\Data\\DATA.xml");
+        }
+
+        private void StartPoints()
+        {
+            points.Add(new Point(200, 200));
+            points.Add(new Point(400, 200));
+            points.Add(new Point(450, 400));
+            points.Add(new Point(400, 600));
+            points.Add(new Point(200, 600));
+        }
+
+        public void DrawPoints()
+        {
+            foreach (var p in points)
+            {
+                Ellipse ellipse = new Ellipse
+                {
+                    Fill = Brushes.Red,
+                    Width = 8,
+                    Height = 8,
+                };
+                Canvas.SetLeft(ellipse, p.X - ellipse.Width / 2);
+                Canvas.SetTop(ellipse, p.Y - ellipse.Height / 2);
+                canvas.Children.Add(ellipse);
+            }
+            pointListBox.Items.Refresh();
+        }
+        public void ClearPoints()
+        {
+            canvas.Children.Clear();
+            foreach(var poly in shapes)
+            {
+                canvas.Children.Add(poly);
+            }
+        }
+}
+
+[Serializable]
+public class PolygonData
+{
+    public List<Point> points { get; set; }
+    public string FillColor { get; set; }
+}
+
+[Serializable]
+[XmlInclude(typeof(PolygonData))]
+public class PolygonList
+{
+    public List<PolygonData> Shapes { get; set; } = new List<PolygonData>();
+}
